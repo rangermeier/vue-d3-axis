ea5fed9e236f1bab15b8827dace92d4b1266430e
Rupert Angermeier <rupert.angermeier@gmail.com>
2026-03-04 10:47:20 +0100

    Update demo
    - place axis so they are fully visible
    - allow to adjust number of ticks and domain

--- demo/App.vue
+++ demo/App.vue
@@ -1,24 +1,60 @@
 <script setup lang="ts">
-import { Axis, axisPropsFromTickScale, TOP, RIGHT, BOTTOM, LEFT, type Orients } from '../src/'
+import { computed, ref } from 'vue';
+import Axis from '../src/axis.vue';
+import { TOP, RIGHT, BOTTOM, LEFT } from '../src/orientation';
+import { axisPropsFromTickScale} from '../src/axisPropsFromTickScale';
 import {scaleLinear} from 'd3-scale';
 
-const scale = scaleLinear().domain([0, 100]).range([0, 500]);
-const orientations: Orients[] = [TOP, RIGHT, BOTTOM, LEFT];
+const nbTicks = ref(10);
+const domainFrom = ref(0);
+const domainTo = ref(100);
+
+const scale = computed(() => scaleLinear().domain([domainFrom.value, domainTo.value]).range([0, 500]));
+const orientations = [TOP, RIGHT, BOTTOM, LEFT];
 </script>
 
 <template>
     <h1>Vue D3 Axis examples</h1>
+    <form>
+        <label>
+            Number of ticks:
+            <input type="number" v-model.number="nbTicks" min="1" />
+        </label>
+        <label>
+            Domain from:
+            <input type="number" v-model.number="domainFrom" />
+        </label>
+        <label>
+            Domain to:
+            <input type="number" v-model.number="domainTo" />
+        </label>
+    </form>
     <article v-for="orient in orientations" :key="orient">
         <h2>Orientation {{orient}}</h2>
-        <svg>
-            <Axis v-bind="axisPropsFromTickScale(scale, 10)" :styles="{orient}" />
+        <svg :class="orient">
+            <Axis
+                v-bind="axisPropsFromTickScale(scale, nbTicks)"
+                :styles="{orient}"
+                class="axis"
+            />
         </svg>
     </article>
 </template>
 
 <style scoped>
 svg {
-    width: 100%;
-    height: 100px;
+    width: 580px;
+    height: 50px;
+}
+.LEFT, .RIGHT {
+    height: 580px;
+}
+.axis {
+    transform: translate(40px, 20px);
+}
+
+label {
+    display: block;
+    margin-bottom: 0.5em;
 }
 </style>
